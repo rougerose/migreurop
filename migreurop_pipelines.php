336dc5412e9e0480ef0dbf5ec64c127cdd78e38e
christophe le drean <christophe@rouge-rose.net>
2013-01-22 19:37:39 +0100

    ubuntu webfont

--- migreurop_pipelines.php
+++ migreurop_pipelines.php
@@ -3,7 +3,7 @@
 
    // javascript
    function migreurop_insert_head($flux){
-      $js = array('javascript/jquery.tr.min.js','javascript/migreurop_public.js');
+      $js = array('javascript/migreurop_public.js');
       foreach ($js as $f) {
          $flux .= '<script src="'.find_in_path($f).'" type="text/javascript"></script>'."\n";
       }
@@ -12,8 +12,7 @@
 
    // css
    function migreurop_insert_head_css($flux) {
-      $flux .= "<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>\n";
-
+      $flux .= "<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,400italic,700italic|Ubuntu+Condensed' rel='stylesheet' type='text/css'>\n";
       $css = find_in_path('css/migreurop.css');
       if ($css) {
          $flux .= "<link rel='stylesheet' href='$css'>\n";
