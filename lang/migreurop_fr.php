57dacf9da6901f12a3059beafb342a3d805f4b46
christophe le drean <christophe@rouge-rose.net>
2013-02-16 14:40:03 +0100

    squelette mot

--- lang/migreurop_fr.php
+++ lang/migreurop_fr.php
@@ -14,6 +14,7 @@
       // I
       'info_document' => 'Document',
       'info_documents' => 'Documents',
+      'info_fichier' => 'Fichier',
       'info_partager' => 'Partager',
       'info_related' => 'Sur le même sujet',
       'info_themes' => 'Thèmes',
@@ -23,6 +24,25 @@
 
       // P
       'info_ps' => 'Post-scriptum',
+
+      // R
+      // R
+      'info_recherche_article' => '1 article',
+      'info_recherche_articles' => '@nb@ articles',
+      'info_recherche_document' => '1 document',
+      'info_recherche_documents' => '@nb@ documents',
+      'info_recherche_label' => 'Recherche',
+      //'recherche_label_cap' => 'RECHERCHE',
+      'info_recherche_mot' => '1 mot-clé',
+      'info_recherche_mots' => '@nb@ mots-clés',
+      'info_recherche_rubrique' => '1 rubrique',
+      'info_recherche_rubriques' => '@nb@ rubriques',
+      'info_recherche_site' => '1 site web',
+      'info_recherche_sites' => '@nb@ sites web',
+      //'recherche_titre' => 'Recherche :',
+      'info_recherche_resultat' => '1 résultat',
+      'info_recherche_resultats' => '@nb@ résultats',
+      'info_recherche_pas_de_resultat' => 'Aucun résultat',
 
       // T
       'translations' => 'Traductions disponibles'
