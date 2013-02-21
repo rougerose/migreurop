985f9f4711e1d3267c981926342bbc27803925e1
christophe le drean <christophe@rouge-rose.net>
2013-02-21 18:26:53 +0100

    sites : - formulaire de tri selon catégorie et pays - composition pour la rubrique sites utiles - site-resume qui peut être utilisé pour la recherche - css : ajustements divers

---
+++ formulaires/tri_sites.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function formulaires_tri_sites_charger($url_resultat = null) {
+   return array(
+      'mots'=>_request('mots')
+   );
+}
+
+function formulaires_tri_sites_verifier($url_resultat = null) {
+
+}
+
+function formulaires_tri_sites_traiter($url_resultat = null) {
+   if (!$url_resultat) {$url_resultat = self();}
+   // Nettoyer l'URL des mots[] qu'elle contiendrait deja
+   $url_resultat = parametre_url($url_resultat,'mots','');
+   // Mettre dans l'URL
+   $url_resultat = parametre_url($url_resultat,'mots',array_unique(_request('mots')));
+   return array('redirect' => $url_resultat);
+}
+
+?>
