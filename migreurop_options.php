659db115bacec4c93391a52866905e6bccaed732
christophe le drean <christophe@rouge-rose.net>
2013-02-25 15:31:12 +0100

    blocs z5 : - suppression extra2 (non utilisé) - ajout breadcrumb

--- migreurop_options.php
+++ migreurop_options.php
@@ -1,6 +1,9 @@
 <?php
    if (!defined("_ECRIRE_INC_VERSION")) return;
 
+   // bloc zcore : extra2 n'est pas utilisé, ajout de breadcrumb
+   $GLOBALS['z_blocs'] = array('content','extra1','breadcrumb','head','head_js','header','footer');
+
    // pas de cache pour la phase développement
    define('_NO_CACHE',1);
 
