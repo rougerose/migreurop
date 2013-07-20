d040a3b6e52e50d797dee793465ff097e69648d8
christophe le drean <christophe@rouge-rose.net>
2013-07-20 18:57:00 +0200

    cache

--- migreurop_options.php
+++ migreurop_options.php
@@ -5,7 +5,7 @@
    $GLOBALS['z_blocs'] = array('content','extra1','breadcrumb','head','head_js','header','page-foot');
 
    // pas de cache pour la phase développement
-   define('_NO_CACHE',1);
+   // define('_NO_CACHE',1);
 
    $GLOBALS['debut_intertitre'] = "\n<h2 class='spip'>";
    $GLOBALS['fin_intertitre'] = "</h2>\n";
