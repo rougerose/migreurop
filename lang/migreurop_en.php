43dd9c321ad76e841f2f0f96781a294f5fbf3394
christophe le drean <christophe@rouge-rose.net>
2013-08-28 18:24:18 +0200

    langues

--- lang/migreurop_en.php
+++ lang/migreurop_en.php
@@ -3,81 +3,80 @@
 
    $GLOBALS[$GLOBALS['idx_lang']] = array(
       // A
-
+      'all_articles'                   => 'All articles',
+      
       // B
-      'by' => 'Par',
-
+      'by'                             => 'By',
+      
       // C
-      'changer_passe' => 'Changer votre mot de passe',
-      'communiques_presse' => 'Communiqués de presse',
-      'contact_address' => 'Our address',
-      'contact_mail' => 'By mail',
-      'contact_us' => 'Contact us',
-      'credits' => 'Made with <a href="http://spip.net">SPIP</a>. Design by <a href="http://rouge-rose.net">christophe le drean</a>.',
-
+      'changer_passe'                  => 'Changer votre mot de passe',
+      'communiques_presse'             => 'Press releases',
+      'contact_address'                => 'Our address',
+      'contact_mail'                   => 'By mail',
+      'contact_us'                     => 'Contact us',
+      'credits'                        => 'Made with <a href="http://spip.net">SPIP</a>. Design by <a href="http://rouge-rose.net">christophe le drean</a>.',
+      
       // G
-      'goto' => 'Go to&hellip;',
-
+      'goto'                           => 'Go to&hellip;',
+      
       // H
-      'home' => 'Accueil',
-
+      'home'                           => 'Home',
+      
       // I
-      'info_all_f' => 'All',
-      'info_all_m' => 'All',
-      'info_category' => 'Catégorie',
-      'info_country' => 'Pays',
-      'info_document' => 'Document',
-      'info_documents' => 'Documents',
-      'info_fichier' => 'Fichier',
-      'info_partager' => 'Partager',
-      'info_related' => 'Sur le même sujet',
-      'info_sort_by' => 'Sort by',
-      'info_themes' => 'Thèmes',
-      'info_traductions' => 'Traductions disponibles',
-
+      'info_all_f'                     => 'All',
+      'info_all_m'                     => 'All',
+      'info_category'                  => 'Category',
+      'info_country'                   => 'Country',
+      'info_document'                  => 'Document',
+      'info_documents'                 => 'Documents',
+      'info_fichier'                   => 'File',
+      'info_partager'                  => 'Share',
+      'info_related'                   => 'Sur le même sujet',
+      'info_sort_by'                   => 'Sort by',
+      'info_themes'                    => 'Themes',
+      'info_traductions'               => 'Traductions disponibles',
+      
       // L
-      'lastest_news' => 'Latest news',
-      'lastest_articles' => 'Lastest articles',
-
+      'lastest_news'                   => 'Latest news',
+      'lastest_articles'               => 'Lastest articles',
+      
       // M
-      'message_404' => 'La page ou le document que vous demandez est introuvable sur le site&hellip;',
-      'more' => 'La suite',
-
+      'message_404'                    => 'La page ou le document que vous demandez est introuvable sur le site&hellip;',
+      'more'                           => 'Read more',
+      
       // N
-      'next' => 'Next',
-
+      'next'                           => 'Next',
+      
       // P
-      'info_ps' => 'Post-scriptum',
-      'previous' => 'Previous',
-
+      'info_ps'                        => 'Post-scriptum',
+      'previous'                       => 'Previous',
+      
       // R
-      'info_recherche_article' => '1 article',
-      'info_recherche_articles' => '@nb@ articles',
-      'info_recherche_document' => '1 document',
-      'info_recherche_documents' => '@nb@ documents',
-      'info_recherche_label' => 'Recherche',
-      //'recherche_label_cap' => 'RECHERCHE',
-      'info_recherche_mot' => '1 mot-clé',
-      'info_recherche_mots' => '@nb@ mots-clés',
-      'info_recherche_rubrique' => '1 rubrique',
-      'info_recherche_rubriques' => '@nb@ rubriques',
-      'info_recherche_site' => '1 site web',
-      'info_recherche_sites' => '@nb@ sites web',
-      //'recherche_titre' => 'Recherche :',
-      'info_recherche_resultat' => '1 résultat',
-      'info_recherche_resultats' => '@nb@ résultats',
+      'info_recherche_article'         => '1 article',
+      'info_recherche_articles'        => '@nb@ articles',
+      'info_recherche_document'        => '1 document',
+      'info_recherche_documents'       => '@nb@ documents',
+      'info_recherche_label'           => 'Search',
+      'info_recherche_mot'             => '1 keyword',
+      'info_recherche_mots'            => '@nb@ keywords',
+      'info_recherche_rubrique'        => '1 section',
+      'info_recherche_rubriques'       => '@nb@ sections',
+      'info_recherche_site'            => '1 web site',
+      'info_recherche_sites'           => '@nb@ web sites',
+      'info_recherche_resultat'        => '1 result',
+      'info_recherche_resultats'       => '@nb@ results',
       'info_recherche_pas_de_resultat' => 'Aucun résultat',
-
+      
       // S
-      'section_map' => 'Section map',
-      'social_facebook' => 'Facebook',
-      'social_follow_us' => 'Follow us',
-      'social_twitter' => "Twitter",
-      'social_mailing_list' => 'Mailing list',
-
-
+      'section_map'                    => 'Section map',
+      'social_facebook'                => 'Facebook',
+      'social_follow_us'               => 'Follow us',
+      'social_twitter'                 => "Twitter",
+      'social_mailing_list'            => 'Mailing list',
+      
+      
       // T
-      'top_page' => 'Top',
-      'translations' => 'Traductions disponibles'
+      'top_page'                       => 'Top',
+      'translations'                   => 'Traductions disponibles'
    );
 ?>
--- lang/migreurop_fr.php
+++ lang/migreurop_fr.php
@@ -3,83 +3,81 @@
 
    $GLOBALS[$GLOBALS['idx_lang']] = array(
       // A
-      'all_articles' => 'Tous les articles',
-
+      'all_articles'                   => 'Tous les articles',
+      
       // B
-      'by' => 'Par',
-
+      'by'                             => 'Par',
+      
       // C
-      'changer_passe' => 'Changer votre mot de passe',
-      'communiques_presse' => 'Communiqués de presse',
-      'contact_address' => 'Toutes nos coordonnées',
-      'contact_mail' => 'Par mail',
-      'contact_us' => 'Nous contacter',
-      'credits' => 'Réalisé avec <a href="http://spip.net">SPIP</a>. Conception graphique par <a href="http://rouge-rose.net">christophe le drean</a>.',
-
+      'changer_passe'                  => 'Changer votre mot de passe',
+      'communiques_presse'             => 'Communiqués de presse',
+      'contact_address'                => 'Toutes nos coordonnées',
+      'contact_mail'                   => 'Par mail',
+      'contact_us'                     => 'Nous contacter',
+      'credits'                        => 'Réalisé avec <a href="http://spip.net">SPIP</a>. Conception graphique par <a href="http://rouge-rose.net">christophe le drean</a>.',
+      
       // G
-      'goto' => 'Aller &agrave;&hellip;',
-
+      'goto'                           => 'Aller &agrave;&hellip;',
+      
       // H
-      'home' => 'Accueil',
-
+      'home'                           => 'Accueil',
+      
       // I
-      'info_all_f' => 'Toutes',
-      'info_all_m' => 'Tous',
-      'info_category' => 'Catégorie',
-      'info_country' => 'Pays',
-      'info_document' => 'Document',
-      'info_documents' => 'Documents',
-      'info_fichier' => 'Fichier',
-      'info_partager' => 'Partager',
-      'info_related' => 'Sur le même sujet',
-      'info_sort_by' => 'Trier par',
-      'info_themes' => 'Thèmes',
-      'info_traductions' => 'Traductions disponibles',
-
+      'info_all_f'                     => 'Toutes',
+      'info_all_m'                     => 'Tous',
+      'info_category'                  => 'Catégorie',
+      'info_country'                   => 'Pays',
+      'info_document'                  => 'Document',
+      'info_documents'                 => 'Documents',
+      'info_fichier'                   => 'Fichier',
+      'info_partager'                  => 'Partager',
+      'info_related'                   => 'Sur le même sujet',
+      'info_sort_by'                   => 'Trier par',
+      'info_themes'                    => 'Thèmes',
+      'info_traductions'               => 'Traductions disponibles',
+      
       // L
-      'lastest_news' => 'Dernières actualités',
-      'lastest_articles' => 'Derniers articles',
-
+      'lastest_news'                   => 'Dernières actualités',
+      'lastest_articles'               => 'Derniers articles',
+      
       // M
-      'message_404' => 'La page ou le document que vous demandez est introuvable sur le site&hellip;',
-      'more' => 'La suite',
-
+      'message_404'                    => 'La page ou le document que vous demandez est introuvable sur le site&hellip;',
+      'more'                           => 'La suite',
+      
       // N
-      'next' => 'Suivant',
-
+      'next'                           => 'Suivant',
+      
       // P
-      'info_ps' => 'Post-scriptum',
-      'previous' => 'Précédent',
-
+      'info_ps'                        => 'Post-scriptum',
+      'previous'                       => 'Précédent',
+      
       // R
-      'info_recherche_article' => '1 article',
-      'info_recherche_articles' => '@nb@ articles',
-      'info_recherche_document' => '1 document',
-      'info_recherche_documents' => '@nb@ documents',
-      'info_recherche_label' => 'Recherche',
-      //'recherche_label_cap' => 'RECHERCHE',
-      'info_recherche_mot' => '1 mot-clé',
-      'info_recherche_mots' => '@nb@ mots-clés',
-      'info_recherche_rubrique' => '1 rubrique',
-      'info_recherche_rubriques' => '@nb@ rubriques',
-      'info_recherche_site' => '1 site web',
-      'info_recherche_sites' => '@nb@ sites web',
-      //'recherche_titre' => 'Recherche :',
-      'info_recherche_resultat' => '1 résultat',
-      'info_recherche_resultats' => '@nb@ résultats',
+      'info_recherche_article'         => '1 article',
+      'info_recherche_articles'        => '@nb@ articles',
+      'info_recherche_document'        => '1 document',
+      'info_recherche_documents'       => '@nb@ documents',
+      'info_recherche_label'           => 'Recherche',
+      'info_recherche_mot'             => '1 mot-clé',
+      'info_recherche_mots'            => '@nb@ mots-clés',
+      'info_recherche_rubrique'        => '1 rubrique',
+      'info_recherche_rubriques'       => '@nb@ rubriques',
+      'info_recherche_site'            => '1 site web',
+      'info_recherche_sites'           => '@nb@ sites web',
+      'info_recherche_resultat'        => '1 résultat',
+      'info_recherche_resultats'       => '@nb@ résultats',
       'info_recherche_pas_de_resultat' => 'Aucun résultat',
-
+      
       // S
-      'section_map' => 'Plan de la rubrique',
-      'social_facebook' => 'Facebook',
-      'social_follow_us' => 'Nous suivre',
-      'social_twitter' => "Twitter",
-      'social_mailing_list' => 'Liste de diffusion',
-      'sort_keyword' => 'Trier par mot-clé',
-
+      'section_map'                    => 'Plan de la rubrique',
+      'social_facebook'                => 'Facebook',
+      'social_follow_us'               => 'Nous suivre',
+      'social_twitter'                 => "Twitter",
+      'social_mailing_list'            => 'Liste de diffusion',
+      'sort_keyword'                   => 'Trier par mot-clé',
+      
       // T
-      'top_news' => 'À la une',
-      'top_page' => 'Haut de page',
-      'translations' => 'Traductions disponibles'
+      'top_news'                       => 'À la une',
+      'top_page'                       => 'Haut de page',
+      'translations'                   => 'Traductions disponibles'
    );
 ?>
